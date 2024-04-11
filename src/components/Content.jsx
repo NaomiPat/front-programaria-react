4550805c0b675b7e9dae7c12b844c3e1986c4f13
NaomiPat <157522382+NaomiPat@users.noreply.github.com>
2024-04-10 23:35:44 -0300

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -14,7 +14,7 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'https://meu-primeiro-projeto-back-ende-1.onrender.com'
+  const baseURL = 'https://meu-primeiro-projeto-back-ende-1.onrender.com/mulheres'
 
   useEffect(() => {
     async function getData() {
